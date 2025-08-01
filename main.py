d7be8ac221602123a12dcc9712329a19e0960c22
brayan-chege <brayanchege43@gmail.com>
2025-08-01 16:35:15 +0300

    expense viewer added

--- main.py
+++ main.py
@@ -7,3 +7,16 @@ with open("expenses.txt", "a") as file:
     file.write(f"{expense_name}: Ksh {expense_amount}\n")
 
     print("Expense saved!")
+
+def view_expenses():
+        try:
+                with open("expenses.txt", "r") as file:
+                            expenses = file.readlines()
+                                        if not expenses:
+                                                        print("No expenses found.")
+                                                                    else:
+                                                                                    print("Your saved expenses:")
+                                                                                                    for line in expenses:
+                                                                                                                        print(line.strip())
+                                                                                                                            except FileNotFoundError:
+                                                                                                                                    print("No expenses found. You haven't added any yet.")
